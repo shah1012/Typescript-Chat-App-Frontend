04b544a6ec2b797b642264b38dc31ddd23465db1
shah1012 <69828183+shah1012@users.noreply.github.com>
2021-06-17 15:14:40 -0500

    finished sidebar styles starting to work on chat page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import "./styles/app.scss";
 import NavbarNotLogged from "./components/Navbar/NavbarNotLogged";
 import NavBarLogged from "./components/Navbar/NavBarLogged/index";
 import SignUpPage from "./pages/SignUp";
+import ChatSideBar from "./components/ChatSideBar/Index";
 
 function App() {
   return (
     <div className="App">
-      <NavbarNotLogged />
+      {/* <NavbarNotLogged /> */}
       {/* <NavBarLogged /> */}
-      <SignUpPage />
+      {/* <SignUpPage /> */}
+      <ChatSideBar />
     </div>
   );
 }
