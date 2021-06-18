6b8efecd2e08e9ede06646ce83df789004c69d62
Satvik Seeram <satvikgsu@gmail.com>
2021-06-18 23:26:42 +0530

    dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       {/* <NavbarNotLogged /> */}
       <NavBarLogged />
-      {/* <SignUpPage /> */}
+      <SignUpPage />
       <ChatSideBar />
     </div>
   );
