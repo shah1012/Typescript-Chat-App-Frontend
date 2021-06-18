158431071fb68e0d82dacb1b3298b7a701976cc5
Satvik Seeram <satvikgsu@gmail.com>
2021-06-18 06:14:24 +0530

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       {/* <NavbarNotLogged /> */}
-      {/* <NavBarLogged /> */}
+      <NavBarLogged />
       {/* <SignUpPage /> */}
       <ChatSideBar />
     </div>
